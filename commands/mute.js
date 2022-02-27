3abf12a39a29e2b7d74d2fc8cdb0c5d3be56d59e
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 18:27:56 +0100

    e

--- commands/mute.js
+++ commands/mute.js
@@ -1,32 +1,32 @@
 const ms = require('ms');
-module.exports ={
+module.exports = {
     name: 'mute',
-    description: 'Mute a user in the guild.',
-    execute(message, args){
+    description: "This mutes a member",
+    execute(message, args) {
         const target = message.mentions.users.first();
-        if(target){
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Server Ruiner');
+        if (target) {
 
-            let memberTarget = message.guild.members.cache.get(target.id)
-            
-            if(!args[1]){
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+
+            let memberTarget = message.guild.members.cache.get(target.id);
+
+            if (!args[1]) {
                 memberTarget.roles.remove(mainRole.id);
                 memberTarget.roles.add(muteRole.id);
-                message.channel.send(`<@${memberTarget.user.id}> has been muted.`)
+                message.channel.send(`<@${memberTarget.user.id}> has been muted`);
                 return
             }
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms((args[1]))}.`)
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
 
-            setTimeout(function(){
+            setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
                 memberTarget.roles.add(mainRole.id);
-                
-            }, ms(args)[1]);
-        } else{
-            message.channel.send("I couldn't find that user.")
+            }, ms(args[1]));
+        } else {
+            message.channel.send('Cant find that member!');
         }
     }
 }
