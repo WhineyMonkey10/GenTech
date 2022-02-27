53192683a4b9c162414f1a2cbaaa927bb84d19e2
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 12:03:08 +0100

    Permissions on kick

--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,20 @@ module.exports ={
     name: 'kick',
     description: 'Kick a user from the guild.',
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarget = message.guild.members.cache.get(member.id);
-            memberTarget.kick()
-            message.channel.send('Member has been kicked.')
-        }else{
-            message.channel.send('Please specicify a member.')
+       
+        if(message.member.roles.cache.has('840967211278532648')){
+            const member = message.mentions.users.first();
+            if(member){
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberTarget.kick()
+                message.channel.send('Member has been kicked.')
+            }else{
+                message.channel.send('Please specicify a member.')
+            }
+        } else{
+            message.channel.send('You do not have sufficient permissions to execute this command')
+        }
+
+
         }
     }
-}
