7ca2c83415d0d55e49b36d325e21a21cd5b6ee98
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 12:24:56 +0100

    re

---
+++ commands/alphx.js
@@ -0,0 +1,7 @@
+module.exports ={
+    name: 'alphx',
+    description: 'Alphx',
+    execute(message, args){
+        message.channel.send('WhineyMonkey10 had no choice, but to add a command for <@882409450474975242>');
+    }
+}
