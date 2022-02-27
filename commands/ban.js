029090a5ccb944adc98b894b57657c732818e935
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 17:29:56 +0100

    ew

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ module.exports ={
     description: 'Bans a user from the guild.',
     execute(message, args){
         const staffroleskick = ('840967208200175636', '840967208896167996', '840967209354264627', '840967209354264627', '840980700613836801', '840967210365485078', '840967211278532648')
-        if(message.member.roles.cache.has(staffids)){
+        if(message.member.roles.cache.has(staffroleskick)){
             const member = message.mentions.users.first();
             if(member){
                 const memberTarget = message.guild.members.cache.get(member.id);
