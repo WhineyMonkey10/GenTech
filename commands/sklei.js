1dd1710b8f2713b85e2f1b385e1ba7b6fc049f89
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 11:19:03 +0100

    ea#

--- commands/sklei.js
+++ commands/sklei.js
@@ -1,7 +1,7 @@
 module.exports ={
-    name: 'skeli',
-    description: 'UwU.',
+    name: 'sklei',
+    description: 'UwU',
     execute(message, args){
-        message.channel.send('sklei is the better UwU than whineymonkey10');
+        message.channel.send('EASPORTS');
     }
 }
