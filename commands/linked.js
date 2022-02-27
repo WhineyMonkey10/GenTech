9b89882d68c10a7f94e8293f421541ed49048824
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 11:11:47 +0100

    e

--- commands/linked.js
+++ commands/linked.js
@@ -1,5 +1,5 @@
 module.exports ={
-    name: 'link',
+    name: 'linked',
     description: 'Corrects user whilst trying to link.',
     execute(message, args){
         message.channel.send('To link your account please DM <@930141476019662888>. Also, delete your message if it contains your linking code, since people could link to Minecraft as you.');
