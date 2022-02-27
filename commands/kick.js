ca9cbecd714a276379b39f91ceb815c372fe5a16
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-27 15:06:55 +0100

    perms hope fixed

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports ={
     name: 'kick',
     description: 'Kicks a user from the guild.',
     execute(message, args){
-    const staffroles = ('840967212039012393', '840967211278532648', '840967210365485078', '840980700613836801', '840967209354264627', '840967208896167996', '840967208200175636')
+    const staffroles = ('840967208200175636', '840967208896167996', '840967209354264627', '840980700613836801', '840967210365485078', '840967211278532648', '840967212039012393')
         if(message.member.roles.cache.has(staffroles)){
             const member = message.mentions.users.first();
             if(member){
