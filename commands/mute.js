b71c308561432c1d90fc87890206f79efdac6543
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-02-28 19:04:22 +0100

    e

--- commands/mute.js
+++ commands/mute.js
@@ -6,27 +6,27 @@ module.exports = {
         const target = message.mentions.users.first();
         if (target) {
 
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Server Ruiner');
 
             let memberTarget = message.guild.members.cache.get(target.id);
 
             if (!args[1]) {
                 memberTarget.roles.remove(mainRole.id);
                 memberTarget.roles.add(muteRole.id);
-                message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+                message.channel.send(`<@${memberTarget.user.id}> has been muted.`);
                 return
             }
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}.`);
 
             setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
                 memberTarget.roles.add(mainRole.id);
             }, ms(args[1]));
         } else {
-            message.channel.send('Cant find that member!');
+            message.channel.send('I could not find that member.');
         }
     }
 }
