e9811bab52c7b95f1f3bc5e9e5b03f0e9b036050
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-03-04 17:34:00 +0100

    e

--- commands/syco.js
+++ commands/syco.js
@@ -1,6 +1,6 @@
 module.exports ={
     name: 'syco',
-    description: 'Command for Syco.',
+    description: 'Command for syco.',
     execute(message, args){
         message.channel.send('<@946455825684258857> is the best and should get his mod back.');
     }
